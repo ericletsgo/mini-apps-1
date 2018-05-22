1f8ffb729b901556bf138ed3b8d67175c3c1c0c3
Eric Wang <eric.wang1126@gmail.com>
2018-05-21 17:12:38 -0700

    Html and some functionality

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,38 @@
+var board =
+[
+  [0, 0], [0, 1], [0, 2],
+  [1, 0], [1, 1], [1, 2],
+  [2, 0], [2, 1], [2, 2]
+];
 
+function togglePiece() {
+  var move = 1;
+  if (move % 2 !== 0) {
 
+    move++;
+  } else {
+
+    move++;
+  }
+  checkWinCondition();
+}
+
+function reset() {
+  board =
+  [
+    [0, 0], [0, 1], [0, 2],
+    [1, 0], [1, 1], [1, 2],
+    [2, 0], [2, 1], [2, 2]
+  ];
+}
+
+function checkWinCondition(startingPoint) {
+
+}
+
+function checkStalemate() {
+
+}
+
+document.getElementById('grid').addEventListener('click', togglePiece);
+document.getElementById('reset').addEventListener('click', reset);
---
+++ challenge_1/tictactoespec.js
@@ -0,0 +1,32 @@
+//tests
+it('should have a valid game board', function() {
+
+});
+
+it('should have X player piece', function() {
+
+});
+
+it('should have O player piece', function() {
+
+});
+
+it('should only check X and O player pieces for win condition', function() {
+
+});
+
+it('should check if piece placed is valid', function() {
+
+});
+
+it('should check for win condition after four pieces have been placed on the board', function() {
+
+});
+
+it('should check for stalemate after six pieces have been placed on the board', function() {
+
+});
+
+it('should have a reset button that when clicked should reset to original state of board', function() {
+
+});
