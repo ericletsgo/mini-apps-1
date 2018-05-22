6c5fe8f64cc2c2a4987e0cbaae658e44b9da007d
Eric Wang <eric.wang1126@gmail.com>
2018-05-22 14:49:48 -0700

    added skeleton for server and client code

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,31 +1,37 @@
-var app = {
-  post: function() {
-    $.ajax({
-      url: '127.0.0.1',
-      method: 'GET', 'POST', 'PUT',
-      success: function(data) {
+$(document).ready() {
+  var app = {
+    post: function(query) {
+      $.ajax({
+        url: 'http://127.0.0.1:8080/',
+        method: 'POST',
+        data: query,
+        success: function(data) {
+          console.log('success!');
+        },
+        error: function(data) {
+          console.log('failed');
+        },
+      });
+    },
 
-      },
-      error: function(data) {
+    get: function() {
+      $.ajax({
+        url: 'http://127.0.0.1:8080/',
+        method: 'GET',
+        success: function(data) {
 
-      },
-    });
-  },
+        },
+        error: function(data) {
 
-  get: function() {
-    $.ajax({
-      url: '127.0.0.1',
-      method: 'GET', 'POST', 'PUT',
-      success: function(data) {
-
-      },
-      error: function(data) {
-
-      },
-    });
+        },
+      });
+    }
   }
-}
 
-var submit = function() {
-
+  $('#button').click(function (event) {
+    console.log(event);
+    console.log(document.getElementById('testField').val());
+    event.preventDefault();
+    // console.log($('input:first').val());
+  });
 }
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,1 +1,20 @@
 var express = require('express');
+var paths = require('path');
+const app = express();
+
+app.post('/', (req, res) => {
+  var data = req.body;
+  var csv = '';
+
+  for (var key in data) {
+    var columns = key + ',';
+
+
+  }
+});
+
+app.get('/', (req, res) => {
+
+});
+
+app.listen(8080)
