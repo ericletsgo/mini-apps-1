750cccf9dfc04c383e068cfb2152d77676cb4e1b
Eric Wang <eric.wang1126@gmail.com>
2018-05-21 19:35:50 -0700

    Completed all basic requirements

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,38 +1,71 @@
 var board =
 [
-  [0, 0], [0, 1], [0, 2],
-  [1, 0], [1, 1], [1, 2],
-  [2, 0], [2, 1], [2, 2]
+  '1', '2', '3',
+  '4', '5', '6',
+  '7', '8', '9'
 ];
 
-function togglePiece() {
-  var move = 1;
+var move = 1;
+
+function togglePiece(mouseEvent) {
+  var id = mouseEvent.path[0].id;
+
   if (move % 2 !== 0) {
-
+    board[board.indexOf(id)] = 'X';
+    document.getElementById(id.toString()).innerHTML = 'X';
     move++;
+    checkWinCondition('X');
   } else {
-
+    board[board.indexOf(id)] = 'O';
+    document.getElementById(id.toString()).innerHTML = 'O';
     move++;
+    checkWinCondition('O');
   }
-  checkWinCondition();
+  checkStalemate();
 }
 
 function reset() {
   board =
   [
-    [0, 0], [0, 1], [0, 2],
-    [1, 0], [1, 1], [1, 2],
-    [2, 0], [2, 1], [2, 2]
+    '1', '2', '3',
+    '4', '5', '6',
+    '7', '8', '9'
   ];
+  move = 1;
+  for (var i = 1; i < 10; i++) {
+    document.getElementById(i.toString()).innerHTML = '';
+  }
 }
 
-function checkWinCondition(startingPoint) {
-
+function checkWinCondition(player) {
+  for (var i = 0; i < board.length; i++) {
+    if (board[i] === board[i + 3] && board[i] === board[i + 6]) {
+      setTimeout(function() {alert(player + ' Wins!'); }, 500);
+      setTimeout(function() {reset()}, 1500);
+    }
+    if (board[i] === board[i + 1] && board[i] === board[i + 2]) {
+      setTimeout(function() {alert(player + ' Wins!'); }, 500);
+      setTimeout(function() {reset()}, 1500);
+    }
+  }
+  if (board[0] === board[4] && board[0] === board[8]) {
+    setTimeout(function() {alert(player + ' Wins!'); }, 500);
+    setTimeout(function() {reset()}, 1500);
+  }
+  if (board[2] === board[4] && board[2] === board[6]) {
+    setTimeout(function() {alert(player + ' Wins!'); }, 500);
+    setTimeout(function() {reset()}, 1500);
+  }
 }
 
 function checkStalemate() {
-
+  if (move === 10) {
+    setTimeout(function() {alert('Stalemate!')}, 500);
+    setTimeout(function() {reset()}, 1500);
+  }
 }
 
-document.getElementById('grid').addEventListener('click', togglePiece);
+for (var i = 1; i < 10; i++) {
+  document.getElementById(i.toString()).addEventListener('click', togglePiece);
+}
 document.getElementById('reset').addEventListener('click', reset);
