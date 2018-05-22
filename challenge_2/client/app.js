b1d24655433e38601bc03adfd8b0a684380d7bcb
Eric Wang <eric.wang1126@gmail.com>
2018-05-22 11:47:32 -0700

    Setup repo and added packaged dependencies.

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,31 @@
+var app = {
+  post: function() {
+    $.ajax({
+      url: '127.0.0.1',
+      method: 'GET', 'POST', 'PUT',
+      success: function(data) {
 
+      },
+      error: function(data) {
 
+      },
+    });
+  },
+
+  get: function() {
+    $.ajax({
+      url: '127.0.0.1',
+      method: 'GET', 'POST', 'PUT',
+      success: function(data) {
+
+      },
+      error: function(data) {
+
+      },
+    });
+  }
+}
+
+var submit = function() {
+
+}
