d305cf34b9ebfe9c2f651cc3fa508945f9396c25
Eric Wang <eric.wang1126@gmail.com>
2018-05-23 16:26:31 -0700

    Setup mongodb and added ajax request on client. Also refactored.

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,3 +1,36 @@
+const PageOne = (props) => (
+  <div>
+  <h1>Login Information</h1>
+  <form>
+    Name:<input type="input"/>
+    Email:<input type="input"/>
+    Password:<input type="input"/><input onClick={props.nextView} type="submit" value="Submit"/>
+  </form>
+  </div>
+)
+
+const PageTwo = (props) => (
+  <div>
+  <h1>Shipping Information</h1>
+  <form>
+    Line 1:<input type="input"/>
+    Line 2:<input type="input"/>
+    City:<input type="input"/>State:<input type="input"/>
+    Zip Code:<input type="input"/><input onClick={props.nextView} type="submit" value="Submit"/>
+  </form>
+  </div>
+);
+
+const PageThree = (props) => (
+  <div>
+  <h1>Credit Card Information</h1>
+  <form>
+    Credit Card Number:<input type="input"/>
+    Exp. Date:<input type="input" maxlength="4"/>CVV:<input type="input"/>
+    Billing Zip Code: <input type="input" maxlength="5"/><input onClick={props.nextView} type="submit" value="Submit"/>
+    </form>
+  </div>
+);
 
 class App extends React.Component {
   constructor(props) {
@@ -8,10 +41,27 @@ class App extends React.Component {
     this.nextView = this.nextView.bind(this);
   }
 
-  nextView() {
-    this.setState({page: this.state.page + 1});
+  parseUserData(data) {
+
   }
 
+  nextView(userInput) {
+    this.setState({page: this.state.page + 1});
+    if (this.state.page !== 0) {
+      $.ajax({
+        url: 'http://127.0.0.1:8000/',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(parseUserData(userInput)),
+        success: function() {
+
+        },
+        error: function() {
+
+        }
+      });
+    }
+  }
 
   render() {
     if (this.state.page === 0) {
@@ -23,76 +73,18 @@ class App extends React.Component {
     }
     if (this.state.page === 1) {
       return(
-        <div>
-        <h1>Login Information</h1>
-        <form>
-          Name:<input type="input"/>
-          Email:<input type="input"/>
-          Password:<input type="input"/><input onClick={this.nextView} type="submit" value="Submit"/>
-        </form>
-        </div>
-      )
+        <PageOne nextView={this.nextView.bind(this)}/>
+      );
     }
     if (this.state.page === 2) {
       return(
-        <div>
-        <h1>Shipping Information</h1>
-        <form>
-          Line 1:<input type="input"/>
-          Line 2:<input type="input"/>
-          City:<input type="input"/>State:<input type="input"/>
-          Zip Code:<input type="input"/><input onClick={this.nextView} type="submit" value="Submit"/>
-        </form>
-        </div>
-      )
+        <PageTwo nextView={this.nextView.bind(this)}/>
+      );
     }
     if (this.state.page === 3) {
       return(
-        <div>
-        <h1>Credit Card Information</h1>
-        <form>
-          Credit Card Number:<input type="input"/>
-          Exp. Date:<input type="input" maxLength="4"/>CVV:<input type="input"/>
-          Billing Zip Code: <input type="input" maxLength="5"/><input onClick={this.nextView} type="submit" value="Submit"/>
-        </form>
-        </div>
-      )
+        <PageThree nextView={this.nextView.bind(this)}/>
+      );
     }
   }
 }
-//
-// pageOne = () => {
-//   return (
-//     <div>
-//     <h1>Login Information</h1>
-//     <form>
-//       Name:<input type="input"/>
-//       Email:<input type="input"/>
-//       Password:<input type="input"/><input onClick={this.nextView} type="submit" value="Submit"/>
-//     </form>
-//     </div>
-//   )
-// }
-//
-// pageTwo = () => (
-//   <div>
-//   <h1>Shipping Information</h1>
-//   <form>
-//   Line 1:<input type="input"/>
-//   Line 2:<input type="input"/>
-//   City:<input type="input"/>State:<input type="input"/>
-//   Zip Code:<input type="input"/><input onClick={this.nextView} type="submit" value="Submit"/>
-//   </form>
-//   </div>
-// );
-//
-// pageThree = () => (
-//   <div>
-//   <h1>Credit Card Information</h1>
-//   <form>
-//   Credit Card Number:<input type="input"/>
-//   Exp. Date:<input type="input" maxlength="4"/>CVV:<input type="input"/>
-//   Billing Zip Code: <input type="input" maxlength="5"/><input onClick={this.nextView} type="submit" value="Submit"/>
-//   </form>
-//   </div>
-// );
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static('public'));
+
+// app.post()
 
 app.listen(8000);
